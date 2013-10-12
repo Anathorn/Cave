2c385bddd8439c97f79662aff13b080d8d74cc93
unknown <maldo.19@hotmail.com>
2013-10-12 17:18:58 -0300

    animations and other stuffs

--- Assets/Scripts/Player_Script.js
+++ Assets/Scripts/Player_Script.js
@@ -1,13 +1,20 @@
 #pragma strict
 
 
-var speed : int = 5;
-var fall : boolean = false;
-var fallspeed : int = 2;
+var speed : int = 7;
+var fallspeed : float = 0.1;
+var Jumped : boolean = false;
+var Jump_Speed : int = 20;
+var Time_Delay : float =0.3;
+var Next_Frame : float;
+var tiling : Vector2;
 
 
 
 function Start () {
+
+tiling.x = 0;
+tiling.y = 0; 
 
 }
 
@@ -27,18 +34,32 @@ function Update () {
 	
 	}
 
-	if(Input.GetAxis("Vertical") > 0){
+	if(Input.GetAxis("Vertical") > 0 && Jumped == false){
 	
-		transform.Translate(Vector3(0,1 * speed * 2 * Time.deltaTime,0));
+		
+		transform.Translate(Vector3(0, Jump_Speed * Time.deltaTime,0));
 	
 	
 	}
 	
-	if(fall == true){
+	if(Jumped == true){
 	
 	transform.Translate(Vector3(0,(-1) * fallspeed * Time.deltaTime,0));
 	
 	}
+	
+	//animacion :)   #region animation
+	if(Time.time > Next_Frame){
+	
+		tiling = tiling + Vector2( 0.25, 0 );
+		tiling.x = tiling.x%1;
+		renderer.material.SetTextureOffset("_MainTex",tiling);
+		Next_Frame = Time.time + Time_Delay;
+	}	
+	//#endregion 
+	
+	
+
 
 
 
@@ -49,7 +70,7 @@ function OnCollisionEnter(col: Collision){
 
 	if(col.gameObject.tag == "Platform"){
 	
-	fall = false;
+	Jumped = false;
 	
 	
 	}
@@ -61,8 +82,8 @@ function OnCollisionEnter(col: Collision){
 
 function OnCollisionExit(col: Collision){
 
-	fall = true;
-
+	
+Jumped = true;
 
 
 
